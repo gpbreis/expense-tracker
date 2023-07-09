9db0fc7cb0a7f17b2263fecbe9e809cfc4cd2d28
Gustavo P. Breis <gpbreis@gmail.com>
2023-07-08 23:56:03 -0300

    feat: adding expenses list

---
+++ src/main/java/com/expensetracker/controller/ExpenseController.java
@@ -0,0 +1,28 @@
+package com.expensetracker.controller;
+
+import com.expensetracker.dto.ExpenseDto;
+import com.expensetracker.service.ExpenseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ExpenseController {
+
+    private ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("/expenses")
+    public String listExpenses(Model model) {
+
+        List<ExpenseDto> expenses = expenseService.findAllExpenses();
+        model.addAttribute("expenses", expenses);
+        return "expenses-list";
+    }
+
+}
