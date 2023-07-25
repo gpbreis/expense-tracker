b785adae69b066185096a8c3bcbd16a33c8779ce
Gustavo P. Breis <gpbreis@gmail.com>
2023-07-24 23:58:17 -0300

    adding security configurations

--- src/main/java/com/expensetracker/security/SecurityConfig.java
+++ src/main/java/com/expensetracker/security/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.expensetracker.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
+                    try {
+                        auth
+                                .requestMatchers("/login", "/register", "/register/save")
+                                .permitAll()
+                                .and()
+                                .formLogin(form -> form
+                                        .loginPage("/login")
+                                        .defaultSuccessUrl("/persons")
+                                        .loginProcessingUrl("/login")
+                                        .failureUrl("/login?error=true")
+                                        .permitAll())
+                                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                );
+        return http.build();
+    }
 }
