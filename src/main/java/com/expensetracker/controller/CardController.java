80ae9545ba3a21c040529998617da982a168e21c
Gustavo P. Breis <gpbreis@gmail.com>
2023-08-01 23:58:30 -0300

    testing changes on creating cards

--- src/main/java/com/expensetracker/controller/CardController.java
+++ src/main/java/com/expensetracker/controller/CardController.java
@@ -1,10 +1,17 @@
 package com.expensetracker.controller;
 
 import com.expensetracker.dto.CardDto;
+import com.expensetracker.dto.PersonDto;
+import com.expensetracker.model.Card;
 import com.expensetracker.service.CardService;
+import com.expensetracker.service.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,9 +19,11 @@ import java.util.List;
 public class CardController {
 
     private CardService cardService;
+    private PersonService personService;
 
-    public CardController(CardService cardService) {
+    public CardController(CardService cardService, PersonService personService) {
         this.cardService = cardService;
+        this.personService = personService;
     }
 
     @GetMapping("/cards")
@@ -26,8 +35,22 @@ public class CardController {
     }
 
     @GetMapping("/cards/create")
-    public String create(Model model) {
+    public String createCard(Model model) {
+        Card card = new Card();
+        List<PersonDto> persons = personService.findAllPersons();
+        model.addAttribute("persons", persons);
+        model.addAttribute("card", card);
+        return "create-card";
+    }
 
+    @PostMapping("/cards/create")
+    public String createCard(@Valid @ModelAttribute("card") CardDto cardDto, BindingResult result, Model model) {
+        List<PersonDto> persons = personService.findAllPersons();
+        model.addAttribute("persons", persons);
+        if (result.hasErrors()) {
+            model.addAttribute(cardDto);
+            return "create-card";
+        }
         return "create-card";
     }
 }
